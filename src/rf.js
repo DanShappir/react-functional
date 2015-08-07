c14a49b9737c4713d331e786fbf7124a9dad1dce
Dan Shappir <dansh@wix.com>
2015-08-08 01:48:42 +0300

    Cleaner module

--- src/rf.js
+++ src/rf.js
@@ -1,39 +1,41 @@
 /**
  * Created by Dan_Shappir on 7/28/15.
  */
-'use strict';
+const RF = (function () {
+    'use strict';
 
-const React = require('react');
+    const React = require('react');
 
-const RF = Object.create(Object.prototype, {
-    NO_CHANGE: {
-        value: <div>NO CHANGE</div>
-    },
-    create: {
-        writable: true,
-        value(render) {
-            let result = RF.NO_CHANGE;
-            let spec = {
-                displayName: render.displayName,
-                proprTypes: render.propTypes
-            };
-            if (render.defaultProps) {
-                spec.getDefaultProps = function () {
-                    return render.defaultProps;
+    return Object.create(Object.prototype, {
+        NO_CHANGE: {
+            value: <div>NO CHANGE</div>
+        },
+        create: {
+            writable: true,
+            value(render) {
+                let result = RF.NO_CHANGE;
+                let spec = {
+                    displayName: render.displayName,
+                    proprTypes: render.propTypes
                 };
+                if (render.defaultProps) {
+                    spec.getDefaultProps = function () {
+                        return render.defaultProps;
+                    };
+                }
+                spec.render = function () {
+                    this.render = function () {
+                        return result !== RF.NO_CHANGE ? result : null;
+                    };
+                    return render(this.props);
+                };
+                spec.shouldComponentUpdate = function (nextProps) {
+                    result = render(nextProps, this.props);
+                    return result !== RF.NO_CHANGE;
+                };
+                return React.createClass(spec);
             }
-            spec.render = function () {
-                this.render = function () {
-                    return result !== RF.NO_CHANGE ? result : null;
-                };
-                return render(this.props);
-            };
-            spec.shouldComponentUpdate = function (nextProps) {
-                result = render(nextProps, this.props);
-                return result !== RF.NO_CHANGE;
-            };
-            return React.createClass(spec);
         }
-    }
-});
+    });
+}());
 export default RF;
